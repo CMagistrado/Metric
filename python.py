a89b5b02849018f45a3f14b42c58b14bcff4f841
real0day <CMagistrado@gmail.com>
2018-04-11 12:57:02 -0700

    commit python

---
+++ python.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+#
+# Name: python.py
+# Description: This program makes GitHub believe this repo is about python.
+import time, os, sys
+print("You imported time, os, and sys.")
